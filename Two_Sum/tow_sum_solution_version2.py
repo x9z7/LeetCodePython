211fb0c7de462ba913681998760b71547d9b3582
x9z7 <x9z7@proton.me>
2025-09-29 21:56:17 +0530

    Update tow_sum_solution_version2.py
    This version uses HashMap and has the complexity of O(n)

---
+++ Two_Sum/tow_sum_solution_version2.py
@@ -0,0 +1,11 @@
+# Using HashMap
+# O(n)
+# Using dictionary to store values that we have seen
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
